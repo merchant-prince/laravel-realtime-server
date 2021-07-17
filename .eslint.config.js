7372cd3d7f215f64f96dfe1e66be1270e9e6fdd5
Harivansh <hello@harivan.sh>
2021-07-17 19:56:26 +0400

    setup development environment

---
+++ .eslint.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+};
---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: 'ts-jest',
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  rootDir: 'tests',
+  testEnvironment: 'node',
+};
