56ed6129d6768b4d1cfd4fd940e809bf599951d1
Harivansh <hello@harivan.sh>
2021-07-21 16:05:23 +0400

    configure rollup to generate declaration files

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  bail: true,
   preset: 'ts-jest',
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   rootDir: 'tests',
--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,27 @@
 import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: './src/index.ts',
-  output: {
-    file: './dist/realtime-server.js',
-    format: 'esm',
+const config = [
+  {
+    input: './src/index.ts',
+    output: {
+      file: './dist/realtime.js',
+      format: 'esm',
+    },
+    plugins: [
+      typescript({ tsconfig: './tsconfig.json' }),
+    ],
   },
-  plugins: [
-    typescript(),
-  ],
-};
+  {
+    input: './dist/types/src/index.d.ts',
+    output: {
+      file: './dist/realtime.d.ts',
+      format: 'esm',
+    },
+    plugins: [
+      dts(),
+    ],
+  },
+];
+
+export default config;
