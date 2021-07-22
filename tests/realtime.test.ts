25874f169798a74008d70bf5373cec98fbc3316d
Harivansh <hello@harivan.sh>
2021-07-22 10:12:24 +0400

    add unsubscription test

--- tests/realtime.test.ts
+++ tests/realtime.test.ts
@@ -12,18 +12,53 @@ describe('testing the Realtime class', () => {
   });
 
   // constructor
-  test("the client's socket joins a room identified by the 'channelName' when a 'subscribe' event is sent to the server", async () => {
+  test("the client's 'server socket' joins a room identified by the 'channelName' when a 'subscribe' event is sent to the server from the 'client socket'", async () => {
+    expect.assertions(2);
+
     const { socketPairs, socketIoServer } =
       await setupRealtimeServerAndSocketIoClients(1);
     const channelName = 'OneTwo';
 
     await new Promise<void>((resolve) => {
+      socketPairs[0]?.server.prependListener('subscribe', () => {
+        expect(socketPairs[0]?.server.rooms.has(channelName)).toBeFalsy();
+      });
+
       socketPairs[0]?.server.on('subscribe', () => {
         expect(socketPairs[0]?.server.rooms.has(channelName)).toBeTruthy();
         resolve();
       });
 
       socketPairs[0]?.client.emit('subscribe', channelName);
+    });
+
+    socketPairs.forEach(({ client }) => client.close());
+    socketIoServer.close();
+  });
+
+  test("the client's 'server socket' leaves a room identified by the 'channelName' when an 'unsubscribe' event is sent to the server from the 'client socket'", async () => {
+    expect.assertions(2);
+
+    const { socketPairs, socketIoServer } =
+      await setupRealtimeServerAndSocketIoClients(1);
+    const channelName = 'OneTwo';
+
+    await new Promise<void>((resolve) => {
+      socketPairs[0]?.server.on('subscribe', resolve);
+      socketPairs[0]?.client.emit('subscribe', channelName);
+    });
+
+    await new Promise<void>((resolve) => {
+      socketPairs[0]?.server.prependListener('unsubscribe', () => {
+        expect(socketPairs[0]?.server.rooms.has(channelName)).toBeTruthy();
+      });
+
+      socketPairs[0]?.server.on('unsubscribe', (channelName) => {
+        expect(socketPairs[0]?.server.rooms.has(channelName)).toBeFalsy();
+        resolve();
+      });
+
+      socketPairs[0]?.client.emit('unsubscribe', channelName);
     });
 
     socketPairs.forEach(({ client }) => client.close());
