5e6c2053f51b8060c0f521f455faa0c560d38d88
Harivansh <hello@harivan.sh>
2021-07-22 05:34:41 +0400

    setup before & after hooks

--- tests/realtime.test.ts
+++ tests/realtime.test.ts
@@ -1,6 +1,24 @@
 import Realtime from '../src/realtime';
+import { setupRealtimeServerAndSocketIoClient } from './utilities/setup';
+import { Server as SocketIoServer, Socket as ServerSocket } from 'socket.io';
+import { Socket as ClientSocket } from 'socket.io-client';
 
 describe('testing the Realtime class', () => {
+  let realtime: Realtime;
+  let socketIoServer: SocketIoServer;
+  let serverSocket: ServerSocket;
+  let clientSocket: ClientSocket;
+
+  beforeEach(async () => {
+    ({ serverSocket, clientSocket, socketIoServer, realtime } =
+      await setupRealtimeServerAndSocketIoClient());
+  });
+
+  afterEach(() => {
+    socketIoServer.close();
+    clientSocket.close();
+  });
+
   // Realtime.isPresenceChannel
   it('correct determines whether a channel is a presence channel', () => {
     const presenceChannel = 'presence-One.Two';
@@ -8,5 +26,12 @@ describe('testing the Realtime class', () => {
 
     expect(Realtime['isPresenceChannel'](presenceChannel)).toBeTruthy();
     expect(Realtime['isPresenceChannel'](nonPresenceChannel)).toBeFalsy();
+  });
+
+  // constructor
+  it('sets up realtime successfully', () => {
+    realtime; // @todo: setup multiple clients.
+    expect(serverSocket.connected).toBeTruthy();
+    expect(clientSocket.connected).toBeTruthy();
   });
 });
