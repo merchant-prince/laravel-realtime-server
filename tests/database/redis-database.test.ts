63ecd977a29899752a8df9f1e8be4b976c9dd187
Harivansh <hello@harivan.sh>
2021-07-28 08:14:38 +0400

    switch redis' mock to ioredis-mock

--- tests/database/redis-database.test.ts
+++ tests/database/redis-database.test.ts
@@ -1,6 +1,5 @@
-import IORedis from 'ioredis';
 import RedisDatabase from '../../src/database/redis-database';
-import Redis from '../utilities/mocks/redis';
+import RedisMock from 'ioredis-mock';
 
 describe('testing the RedisDatabase class', () => {
   it("creates a (unique) 'user-key' from the channel name and user data provided", () => {
@@ -25,9 +24,11 @@ describe('testing the RedisDatabase class', () => {
       eyeCount: 1,
     };
     const redisDatabase = new RedisDatabase(
-      new Redis({
-        [socketId]: JSON.stringify(userData),
-      }) as unknown as IORedis.Redis
+      new RedisMock({
+        data: {
+          [socketId]: JSON.stringify(userData),
+        },
+      })
     );
 
     expect(await redisDatabase.getUserDataFromSocketId(socketId)).toEqual(
@@ -38,9 +39,7 @@ describe('testing the RedisDatabase class', () => {
   it("returns 'null' for a non-existent socket id", async () => {
     expect.assertions(1);
 
-    const redisDatabase = new RedisDatabase(
-      new Redis() as unknown as IORedis.Redis
-    );
+    const redisDatabase = new RedisDatabase(new RedisMock());
 
     expect(
       await redisDatabase.getUserDataFromSocketId('non-existent-socket.id')
@@ -55,12 +54,12 @@ describe('testing the RedisDatabase class', () => {
       id: 555,
       address: 'over there',
     };
-    const redis = new Redis();
-    const redisDatabase = new RedisDatabase(redis as unknown as IORedis.Redis);
+    const redis = new RedisMock();
+    const redisDatabase = new RedisDatabase(redis);
 
     await redisDatabase.associateUserDataToSocketId(userData, socketId);
 
-    expect(redis['database'][socketId]).toBe(JSON.stringify(userData));
+    expect(await redis.get(socketId)).toBe(JSON.stringify(userData));
   });
 
   it("dissociates a user's data from a socket id", async () => {
@@ -70,89 +69,80 @@ describe('testing the RedisDatabase class', () => {
     const userData = {
       email: 'one@one.one',
     };
-    const redis = new Redis({
-      [socketId]: JSON.stringify(userData),
+    const redis = new RedisMock({
+      data: {
+        [socketId]: JSON.stringify(userData),
+      },
     });
-    const redisDatabase = new RedisDatabase(redis as unknown as IORedis.Redis);
+    const redisDatabase = new RedisDatabase(redis);
 
     await redisDatabase.dissociateUserDataFromSocketId(socketId);
 
-    expect(redis['database'][socketId]).toBeUndefined();
+    expect(await redis.get(socketId)).toBeNull();
   });
 
   it("increases or creates a user's socket count", async () => {
-    expect.assertions(5);
+    expect.assertions(3);
 
     const channelName = 'le-channel-one';
     const userData = {
       id: 445,
       name: 'Four Four Five',
     };
-    const redis = new Redis();
-    const redisDatabase = new RedisDatabase(redis as unknown as IORedis.Redis);
+    const redis = new RedisMock();
+    const redisDatabase = new RedisDatabase(redis);
 
     expect(
-      RedisDatabase['userKey'](channelName, userData) in redis['database']
-    ).toBeFalsy();
-
+      await redis.get(RedisDatabase['userKey'](channelName, userData))
+    ).toBeNull();
     expect(
       await redisDatabase.createOrIncreaseUserSocketCount(userData, channelName)
     ).toBe(1);
     expect(
-      redis['database'][RedisDatabase['userKey'](channelName, userData)]
-    ).toBe(1);
-
-    expect(
       await redisDatabase.createOrIncreaseUserSocketCount(userData, channelName)
-    ).toBe(2);
-    expect(
-      redis['database'][RedisDatabase['userKey'](channelName, userData)]
     ).toBe(2);
   });
 
   it("decreases or removes a user's socket count", async () => {
-    expect.assertions(4);
+    expect.assertions(2);
 
     const channelName = 'le-channel-two';
     const userData = {
       id: 1001,
       name: 'One Two',
     };
-    const redis = new Redis({
-      [RedisDatabase['userKey'](channelName, userData)]: 2,
+    const redis = new RedisMock({
+      data: {
+        [RedisDatabase['userKey'](channelName, userData)]: 2,
+      },
     });
-    const redisDatabase = new RedisDatabase(redis as unknown as IORedis.Redis);
+    const redisDatabase = new RedisDatabase(redis);
 
     expect(
       await redisDatabase.removeOrDecreaseUserSocketCount(userData, channelName)
     ).toBe(1);
-    expect(
-      redis['database'][RedisDatabase['userKey'](channelName, userData)]
-    ).toBe(1);
-
     expect(
       await redisDatabase.removeOrDecreaseUserSocketCount(userData, channelName)
     ).toBe(0);
-    expect(
-      RedisDatabase['userKey'](channelName, userData) in redis['database']
-    ).toBeFalsy();
   });
 
   it("retrieves a channel's members", async () => {
     expect.assertions(1);
 
-    const channelName = 'NutPhux';
+    const channelName = 'Squee-Spleen-and-Spoon';
     const channelMembers = [
       { id: 1, name: 'One' },
       { id: 2, name: 'Two' },
       { id: 3, name: 'Three' },
     ];
-    const redis = new Redis({
-      [channelName]: new Set(
-        channelMembers.map((member) => JSON.stringify(member))
-      ),
+    const redis = new RedisMock({
+      data: {
+        [channelName]: new Set(
+          channelMembers.map((member) => JSON.stringify(member))
+        ),
+      },
     });
-    const redisDatabase = new RedisDatabase(redis as unknown as IORedis.Redis);
+    const redisDatabase = new RedisDatabase(redis);
 
     expect(await redisDatabase.getChannelMembers(channelName)).toEqual(
       channelMembers
@@ -167,36 +157,42 @@ describe('testing the RedisDatabase class', () => {
       id: 4,
       name: 'Four',
     };
-    const redis = new Redis({
-      [channelName]: new Set(JSON.stringify({ id: 5, name: 'Five' })),
+    const redis = new RedisMock({
+      data: {
+        [channelName]: new Set(JSON.stringify({ id: 5, name: 'Five' })),
+      },
     });
-    const redisDatabase = new RedisDatabase(redis as unknown as IORedis.Redis);
+    const redisDatabase = new RedisDatabase(redis);
 
     await redisDatabase.addUserDataToChannel(userData, channelName);
 
-    expect(redis['database'][channelName]).toContain(JSON.stringify(userData));
+    expect(await redis.smembers(channelName)).toContain(
+      JSON.stringify(userData)
+    );
   });
 
   it("removes a user's data from a set identified by a channel's name", async () => {
     expect.assertions(1);
 
     const channelName = 'FiveSix';
-    const userData = {
+    const userDataToRemove = {
       id: 554,
       name: 'FiveFiveFour',
     };
-    const redis = new Redis({
-      [channelName]: new Set([
-        JSON.stringify({ id: 14, name: 'Fourteen' }),
-        JSON.stringify(userData),
-      ]),
+    const redis = new RedisMock({
+      data: {
+        [channelName]: new Set(JSON.stringify(userDataToRemove)),
+      },
     });
-    const redisDatabase = new RedisDatabase(redis as unknown as IORedis.Redis);
+    const redisDatabase = new RedisDatabase(redis);
 
-    await redisDatabase.removeUserDataFromChannel(userData, channelName);
+    await redisDatabase.removeUserDataFromChannel(
+      userDataToRemove,
+      channelName
+    );
 
-    expect(redis['database'][channelName]).not.toContain(
-      JSON.stringify(userData)
+    expect(redis.smembers(channelName)).not.toContain(
+      JSON.stringify(userDataToRemove)
     );
   });
 });
